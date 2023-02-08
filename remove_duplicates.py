4667f9729ac574e033530bfbe1a5f5c06c93ffc7
ratcode404 <89196953+ratcode404@users.noreply.github.com>
2023-02-08 17:14:54 +0100

    Create remove_duplicates.py

---
+++ remove_duplicates.py
@@ -0,0 +1,12 @@
+def remove_duplicates(file_path):
+    lines_seen = set()
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    with open(file_path, "w") as file:
+        for line in lines:
+            if line not in lines_seen:
+                file.write(line)
+                lines_seen.add(line)
+
+file_path = "file.txt"
+remove_duplicates(file_path)
